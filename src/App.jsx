b408c22e6238984d26ab8a4701ca2d12960aab1b
annu-adi <rashmi.maurya10@gmail.com>
2026-07-07 12:05:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
    useEffect(() =>{
     const fetchUser = async () =>{
       try{
-        const response = await axios.get("http://localhost:4000/api/v1/user/patient/me", {withCredentials:true});
+        const response = await axios.get("https://vercel-dashboard-a.vercel.app/user/patient/me", {withCredentials:true});
         setIsAuthenticated(true);
         setUser(response.data.user);
 
