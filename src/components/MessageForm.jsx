5d0c99f63f834779e7b7fba6ca8ce3045ea5a2eb
annu-adi <rashmi.maurya10@gmail.com>
2026-07-07 12:15:31 +0530

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -20,7 +20,7 @@ const MessageForm =()=>{
   try{
     await axios
     .post(
-     "http://localhost:4000/api/v1/message/send",
+     "https://vercel-dashboard-a.vercel.app/message/send",
       {firstName, lastName, phone,address, email, message},
       {
         withCredentials:true,
