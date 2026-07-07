00ef8660d838a90e5a3dd79ea43f9da7dabc8177
annu-adi <rashmi.maurya10@gmail.com>
2026-07-07 12:31:52 +0530

    Update AppointmentForm.jsx

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -35,7 +35,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        "https://vercel-dashboard-a.vercel.app/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
